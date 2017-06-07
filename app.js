bc285b7731239f8688937a53028a3211de6210b3
Madeline Stevens <stevensm1987@gmail.com>
2017-06-06 18:06:13 -0700

    saving what ive done for my about me so far

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+var userName = prompt('Hey, What is your name?').toUpperCase();
+alert('Very nice to meet you '+ userName + '. I always need new friends.');
+
+//question #1 of yes or no questions
+var city = prompt('Question #1: Is Maddy from Dallas?').toUpperCase();
+  if (city === 'NO') {
+     alert ("Correct!");
+  } else {
+   alert ("Incorrect...moving on!");
+
+};
+
+//question #2 of yes or no questions
+var leastFavoriteFood = prompt ('Question #2: Does Maddy like french toast?').toUpperCase();
+if  (leastFavoriteFood=== 'NO') {
+  alert ("Corret!");
+}
+else {
+  alert ("Incorrect...moving on!")
+}
+
+//question #3 of yes or no questions
+var school = prompt ('Question #3: Did Maddy go to WWU?').toUpperCase();
+if  (school=== 'YES') {
+  alert ("Corret!");
+}
+else {
+  alert ("Incorrect...moving on!")
+}
+
+//question #4 of yes or no questions
+var job = prompt ('Did Maddy help reset over 3,000 apple IDs in five years?').toUpperCase();
+if  (job=== 'YES') {
+  alert ("Corret!");
+}
+else {
+  alert ("Incorrect...moving on!")
+}
+
+//question #5 of yes or no  questions
+var goals = prompt ("Is there a pug farm in Maddy's future?").toUpperCase();
+if  (goals=== 'YES') {
+  alert ("Corret!");
+}
+else {
+  alert ("Incorrect...moving on!")
+}
+
+
+//question #5 of yes or no questions
+// var
+// if ()
+// if the value of q1 is this
+// else do this
