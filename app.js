26f759565954b2602718a6ccb9e216ce609148e1
Madeline Stevens <stevensm1987@gmail.com>
2017-06-06 20:57:59 -0700

    Deleted some unnecessary spaces in all of my files, moved my console.logs to be positioned under each variable block of code instead of all at the bottom of my file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 var userName = prompt('Hey, before we start the quiz on Maddy, what is your name?').toUpperCase();
 alert('Very nice to meet you '+ userName + '. I always need new friends.');
+console.log('I first asked the user for their name, to help personalize the experience, and then said it was nice to meet them.')
 
 //question #1 of yes or no questions
 var city = prompt('Question #1: Is Maddy from Dallas?').toUpperCase();
@@ -11,6 +12,7 @@ var city = prompt('Question #1: Is Maddy from Dallas?').toUpperCase();
   } else {
    alert ('Incorrect...moving on!');
 };
+console.log('Then I asked my first yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.')
 
 //question #2 of yes or no questions
 var leastFavoriteFood = prompt ('Question #2: Does Maddy like french toast?').toUpperCase();
@@ -20,6 +22,7 @@ if  (leastFavoriteFood=== 'NO') {
 else {
   alert ('Incorrect...moving on!')
 }
+console.log('Then I asked my second yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
 
 //question #3 of yes or no questions
 var school = prompt ('Question #3: Did kids call Maddy snaggletooth Stevens in elementary school?').toUpperCase();
@@ -29,6 +32,7 @@ if  (school=== 'YES') {
 else {
   alert ('Incorrect...moving on!')
 }
+console.log('Then I asked my third yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
 
 //question #4 of yes or no questions
 var job = prompt ('Did Maddy help reset over 3,000 apple IDs at the Apple store?').toUpperCase();
@@ -38,6 +42,7 @@ if  (job=== 'YES') {
 else {
   alert ('Incorrect...moving on!')
 }
+console.log('Then I asked my fourth yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
 
 //question #5 of yes or no  questions
 var goals = prompt ('Is there a pug farm in Maddy\'s future?').toUpperCase();
@@ -47,10 +52,4 @@ if  (goals=== 'YES') {
 else {
   alert ('Incorrect...moving on!')
 }
-
-console.log('I first asked the user for their name, to help personalize the experience, and then said it was nice to meet them.')
-console.log('Then I asked my first yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.')
-console.log('Then I asked my second yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
-console.log('Then I asked my third yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
-console.log('Then I asked my fourth yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
 console.log('Then I asked my fifth yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.')
