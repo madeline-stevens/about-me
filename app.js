164d909de6e7d070598e4b4f720e0f65fd92511c
Madeline Stevens <stevensm1987@gmail.com>
2017-06-08 09:55:09 -0700

    I corrected some typos

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ alert('Very nice to meet you ' + userName + '. I always need new friends.');
 console.log('I first asked the user for their name, to help personalize the experience, and then said it was nice to meet them.');
 
 //question #1 of yes or no questions
-var city = prompt('Question #1: Is Maddy from Dallas?').toUpperCase();
+var city = prompt('Question #1: Is Maddy from Dallas? Yes or No.').toUpperCase();
 if (city === 'NO') {
   alert ('Correct!');
 } else {
   alert ('Incorrect...moving on!');
 };
-console.log('Then I asked my first yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.');
+console.log('Then I asked my first yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on. Yes or No.');
 
 //question #2 of yes or no questions
-var leastFavoriteFood = prompt ('Question #2: Does Maddy like french toast?').toUpperCase();
+var leastFavoriteFood = prompt ('Question #2: Does Maddy like french toast? Yes or No.').toUpperCase();
 if (leastFavoriteFood === 'NO') {
   alert ('Correct!');
 }
 else {
   alert ('Incorrect...moving on!');
 };
-console.log('Then I asked my second yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.');
+console.log('Then I asked my second yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.');
 
 //question #3 of yes or no questions
 var school = prompt ('Question #3: Did kids call Maddy snaggletooth Stevens in elementary school?').toUpperCase();
@@ -31,7 +31,7 @@ if (school === 'YES') {
 else {
   alert ('Incorrect...moving on!');
 };
-console.log('Then I asked my third yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.');
+console.log('Then I asked my third yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.');
 
 //question #4 of yes or no questions
 var job = prompt ('Did Maddy help reset over 3,000 apple IDs at the Apple store?').toUpperCase();
@@ -41,7 +41,7 @@ if (job === 'YES') {
 else {
   alert ('Incorrect...moving on!');
 };
-console.log('Then I asked my fourth yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.');
+console.log('Then I asked my fourth yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.');
 
 //question #5 of yes or no  questions
 var goals = prompt ('Is there a pug farm in Maddy\'s future?').toUpperCase();
@@ -51,14 +51,14 @@ if (goals === 'YES') {
 else {
   alert ('Incorrect...moving on!');
 };
-console.log('Then I asked my fifth yes or no question, if the user answers correctly they get a Corret! confirmation if not they are told to move on.');
+console.log('Then I asked my fifth yes or no question, if the user answers correctly they get a Correct! confirmation if not they are told to move on.');
 
 //question #6- my first numeric input question with four guesses
 var correctNumber = 4;
 var attempts = 4;
 
 while (attempts > 0) {
-  var answer = parseInt(prompt ('You have four attempts to guess how many bones Maddy has broken?'));
+  var answer = parseInt(prompt ('You have four attempts to guess how many bones Maddy has broken?')); //by default a prompt method will turn even a numeric answer into the string data type, parseInt will convert to a true number data type.
   if (answer < correctNumber) {
     alert ('Incorrect! Number too low! Keep trying!'); // I took out attempts--; following this line of code to test whether the counting down would work- this was a success!!!
   }
@@ -68,13 +68,13 @@ while (attempts > 0) {
   }
   else if (answer === correctNumber) {
     alert ('Correct! Most of them are soccer related...⚽️');
-    attempts = 0;
+    attempts = 0; // this is what sets the while loop to 0, thus making it false, and exiting the loop
   }
   else
   alert ('Incorrect! Number too low! Keep trying!');
   attempts--;
 };
-//console.log ('After the four attempts, I do not have a message saying that the user is done!!!')
+console.log ('After the four attempts, I do not have a message saying that the user is done!!!');
 
 //question #7- my second numeric input question
 var whichBone = ['shoulder', 'knee', 'finger', 'wrist'];
@@ -92,7 +92,9 @@ while (guessesLeft > 0) {
     guessesLeft--;
     alert ('Incorrect! You have ' + guessesLeft + ' attempts left! Keep trying!');
     if (guessesLeft === 0) {
-      alert ('And you are out of guesses!');//message if there are attempts left
+      alert ('And you are out of guesses! And done with the quiz!');//message if there are attempts left
+      console.log ('I STILL NEED TO INCLUDE THE LAST MESSAGE ABOUT HOW MANY QUESTIONS THE USER GOT CORRECT! need to add correctAnswers++; , still need to make sure the upperCase is correct??? ');
+      //alert ('userName, You got '+ 'correctAnswers' + ' out of 7 questions correct! Good job!');
     }
   }
 
